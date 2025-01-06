ee0e5d8f392e9f82cd8c9ccfb94f1ae4823dc0d3
Safin133847 <157776203+Safin133847@users.noreply.github.com>
2025-01-06 20:17:12 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,58 @@
-const path = require("path");
-const HtmlWebpackPlugin= require('html-webpack-plugin');
-module.exports = {
-    entry: './src/index.js',
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-    output: {
-        path: path.join(__dirname,"/dist"),
-        filename: "index_bundle.js",
-    },
-    module:{
-        rules: [
-            {
-                test: /\.js$|\.jsx$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: 'style-loader',
-                    },
-                    {
-                        loader: 'css-loader',
-                    },
-                ]
-            },
+module.exports = {
+  entry: './src/index.js',
+
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index_bundle.js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$|\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
         ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html"
-        })
+      },
+      // Add this rule for handling images and other assets
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        use: ['file-loader'],
+      },
     ],
-    resolve: {
-        extensions: ['.js', '.jsx'],
-    }
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 3000,
+    hot: true, 
+    open: true, 
+  },
+
+  mode: 'development',
 };
