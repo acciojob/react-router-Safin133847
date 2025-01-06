3a2997882c95174e0b4e321a5c848ed614f6ed76
Safin133847 <157776203+Safin133847@users.noreply.github.com>
2025-01-06 20:09:19 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom"; 
 
 const Home = () => {
   return <h1>Welcome to the Home Page!</h1>;
